21c2427ff9d966e9b056533266dc911bc4a778d6
Denny <denny-p@users.noreply.github.com>
2025-03-28 20:34:23 +0100

    render Formular

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\GaesteBuchType;
 
 final class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
+
+        $form = $this->createForm(GaesteBuchType::class);
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'formular' => $form,
         ]);
     }
 }
